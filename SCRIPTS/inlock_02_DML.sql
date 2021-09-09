c49f449f2d0806c96f8f6ba74bd53ada11423b0f
Wesley <wmixgamer@gmail.com>
2021-09-09 11:21:10 -0300

    Scripts quase prontos, com DQL a terminar

---
+++ SCRIPTS/inlock_02_DML.sql
@@ -0,0 +1,19 @@
+USE INLOCK_GAMES_MANHA;
+GO
+
+INSERT INTO TIPOUSUARIO (tituloTipoUsuario)
+VALUES ('Administrador'), ('Cliente');
+GO
+
+INSERT INTO ESTUDIO (nomeEstudio)
+VALUES ('Rockstar Studios'), ('Square Enix'), ('Blizzard');
+GO
+
+INSERT INTO USUARIO (idTipoUsuario, email, senha)
+VALUES (1, 'admin@admin.com', 'admin'), (2, 'cliente@cliente.com', 'cliente');
+GO
+
+INSERT INTO JOGOS (idEstudio, nomeJogo, descricao, dataLancamento, valor)
+VALUES 
+(3, 'Diablo 3', 'é um jogo que contém bastante ação e é viciante, seja você um novato ou um fã.', '2012-05-12', '99,00'), 
+(1, 'Red Dead Redemption II', 'jogo eletrônico de ação-aventura western.', '2018-10-26', '120,00');
---
+++ SCRIPTS/inlock_03_DQL.sql
@@ -0,0 +1,22 @@
+USE INLOCK_GAMES_MANHA;
+GO
+
+--Listar todos os Usuários
+SELECT email, senha, idTipoUsuario FROM USUARIO LEFT JOIN TIPOUSUARIO ON TIPOUSUARIO.idTipoUsuario = USUARIO.idTipoUsuario;
+
+--Listar todos os Estúdios 
+SELECT * FROM ESTUDIO;
+
+--Listar todos os jogos
+SELECT nomeEstudio, nomeJogo, descricao, dataLancamento, valor FROM JOGOS LEFT JOIN ESTUDIO ON ESTUDIO.idEstudio = JOGOS.idEstudio;
+
+--Listar todos os jogos e suas respectivos estúdios
+SELECT nomeJogo, nomeEstudio FROM JOGOS LEFT JOIN ESTUDIO ON JOGOS.idEstudio = ESTUDIO.idEstudio;
+
+--Listar todos os estúdios com os respectivos jogos (mesmo que eles não tenham jogos cadastrados)
+SELECT nomeEstudio, nomeJogo FROM ESTUDIO LEFT JOIN JOGOS ON ESTUDIO.idEstudio = JOGOS.idEstudio;
+
+--Buscar um usuário por email e senha (login)
+SELECT email, senha FROM USUARIO WHERE email = 'cliente@cliente.com' AND senha = 'cliente';
+
+--
